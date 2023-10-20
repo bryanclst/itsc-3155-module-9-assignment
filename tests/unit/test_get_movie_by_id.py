2fb86118c9f0f9b749797d0cda2fd443c83fafcb
Nicholas_Zoll <nickzll@yahoo.com>
2023-10-20 04:33:51 -0400

    initial commit, finished unit test for get_movie_by_id

--- tests/unit/test_get_movie_by_id.py
+++ tests/unit/test_get_movie_by_id.py
@@ -1,1 +1,18 @@
 # TODO: Feature 4
+from src.repositories.movie_repository import get_movie_repository
+
+def test_get_movie_by_id():
+    movie_repository = get_movie_repository()
+    movie_repository.clear_db()
+
+    temp_movie = movie_repository.create_movie("Alien","Ridley Scott", 5)
+    result = movie_repository.get_movie_by_id(temp_movie.movie_id)
+    assert result == temp_movie #checks if movie exists
+
+    movie_repository.clear_db()
+    result2 = movie_repository.get_movie_by_id(1)
+    assert result2 == None #checks that function returns None when given a invalid id
+
+
+
+
