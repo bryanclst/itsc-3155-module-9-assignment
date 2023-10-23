44eec0b20772ac00cdff50ec300eff75df64e398
kiana <kianatruong03@gmail.com>
2023-10-22 21:13:23 -0400

    changed the rating to an int

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -7,10 +7,10 @@ movie_repository = get_movie_repository()
 def test_get_all_movies():
     movie_repository.clear_db()
     
-    movie_repository.create_movie('Movie1', 'Director1', '4')
-    movie_repository.create_movie('Movie2', 'Director2', '3')
-    movie_repository.create_movie('Movie3', 'Director3', '2')
-    movie_repository.create_movie('Movie4', 'Director4', '1')
+    movie_repository.create_movie('Movie1', 'Director1', 4)
+    movie_repository.create_movie('Movie2', 'Director2', 3)
+    movie_repository.create_movie('Movie3', 'Director3', 2)
+    movie_repository.create_movie('Movie4', 'Director4', 1)
     
     all_movies = movie_repository.get_all_movies()
     
