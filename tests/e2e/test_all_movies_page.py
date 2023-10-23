db8585f18283d308a6dcec881c38660ad6e8731e
kiana <kianatruong03@gmail.com>
2023-10-23 01:25:42 -0400

    still trying to fix the error code

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -2,6 +2,7 @@
 # Kiana Truong
 
 from src.repositories.movie_repository import get_movie_repository
+import sys
 
 def test_all_movies_page(test_app):
     movie_repository = get_movie_repository()
@@ -13,19 +14,17 @@ def test_all_movies_page(test_app):
     
     #if reponse.data == null%
     if response.status_code == 200:
-        # if response.data:
-        #     assert b'All Movies' in response.data
-        #     assert b'See our list of movie ratings below' in response.data
-        #     assert b'Halloween' in response.data
-        #     assert b'San Diego' in response.data
-        #     assert b'4' in response.data
-            
-        #     assert b'Scary Movie' in response.data
-        #     assert b'San Dino' in response.data
-        #     assert b'2' in response.data
-          
-        # else:
-        #     pass
-        print('aaaaaaa' + str(response.data))
+        if response.data:
+            assert b'All Movies' in response.data
+            assert b'See our list of movie ratings below' in response.data
+            assert b'Halloween' in response.data
+            assert b'San Diego' in response.data
+            assert 4 in response.data
+
+            assert b'Scary Movie' in response.data
+            assert b'San Dino' in response.data
+            assert b'2' in response.data
+        else:
+            pass
     else:
         assert False
