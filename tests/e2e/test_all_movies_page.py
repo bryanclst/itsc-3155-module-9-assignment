4d593d31558a617e9fda6efeb91a7bd5cdd1256b
kiana <kianatruong03@gmail.com>
2023-10-23 01:49:22 -0400

    fixed the issue of Hallowwen not passing the test case

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -2,29 +2,28 @@
 # Kiana Truong
 
 from src.repositories.movie_repository import get_movie_repository
-import sys
 
 def test_all_movies_page(test_app):
     movie_repository = get_movie_repository()
     movie_repository.clear_db()
     
+    movie_repository.create_movie('Halloween', 'San Diego', 4)
+    movie_repository.create_movie('Scary Movie', 'San Dino', 2)
+    
     client = test_app
     
     response = client.get('/movies')
     
-    #if reponse.data == null%
-    if response.status_code == 200:
-        if response.data:
-            assert b'All Movies' in response.data
-            assert b'See our list of movie ratings below' in response.data
-            assert b'Halloween' in response.data
-            assert b'San Diego' in response.data
-            assert 4 in response.data
+    assert response.status_code ==200
+    assert response.data is not None
+    
+    assert b'All Movies' in response.data
+    assert b'See our list of movie ratings below' in response.data
+    assert b'Halloween' in response.data
+    assert b'San Diego' in response.data
+    assert b'4' in response.data
 
-            assert b'Scary Movie' in response.data
-            assert b'San Dino' in response.data
-            assert b'2' in response.data
-        else:
-            pass
-    else:
-        assert False
+    assert b'Scary Movie' in response.data
+    assert b'San Dino' in response.data
+    assert b'2' in response.data
+
